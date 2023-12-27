2a94293d82ee89c99a2916db65ae616f88150137
przbrys <prz.brys@gmail.com>
2023-12-27 12:38:11 +0100

    Project init

---
+++ src/main/java/com/GrowWithMe/GrowWithMe/GrowWithMeApplication.java
@@ -0,0 +1,12 @@
+package com.GrowWithMe.GrowWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowWithMeApplication.class, args);
+	}
+}
